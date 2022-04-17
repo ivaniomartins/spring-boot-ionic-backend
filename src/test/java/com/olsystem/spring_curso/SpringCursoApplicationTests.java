c5e04a082aed3849d996407a555a458bc1dfde44
Ivanio Martins <ivaniomartins1991@gmail.com>
2022-04-17 04:42:39 -0700

    Projeto criado

---
+++ src/test/java/com/olsystem/spring_curso/SpringCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olsystem.spring_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
