bad979434c677792f67c35b3958beaad4256e78d
Ivanio Martins <ivaniomartins1991@gmail.com>
2022-04-18 14:21:46 -0300

    Criando operação de instanciação

--- src/main/java/com/olsystem/spring_curso/SpringCursoApplication.java
+++ src/main/java/com/olsystem/spring_curso/SpringCursoApplication.java
@@ -1,13 +1,32 @@
 package com.olsystem.spring_curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.olsystem.spring_curso.domain.Categoria;
+import com.olsystem.spring_curso.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringCursoApplication {
+public class SpringCursoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
