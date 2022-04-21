80c2f9d934b85bc73ff8c148acaaac7a7a3f7210
Ivanio Martins <ivaniomartins1991@gmail.com>
2022-04-21 14:52:13 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/olsystem/spring_curso/services/ClienteService.java
+++ src/main/java/com/olsystem/spring_curso/services/ClienteService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.olsystem.spring_curso.domain.Categoria;
 import com.olsystem.spring_curso.domain.Cliente;
 import com.olsystem.spring_curso.repositories.ClienteRepository;
 import com.olsystem.spring_curso.services.exceptions.ObjectNotFoundException;
@@ -16,7 +15,7 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
