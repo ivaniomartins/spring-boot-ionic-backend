f4c6e160114080b4e04a40ce87513b088117b4eb
Ivanio Martins <ivaniomartins1991@gmail.com>
2022-05-01 11:46:17 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/olsystem/spring_curso/dto/ClienteDTO.java
+++ src/main/java/com/olsystem/spring_curso/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.olsystem.spring_curso.domain.Cliente;
+import com.olsystem.spring_curso.services.validations.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
