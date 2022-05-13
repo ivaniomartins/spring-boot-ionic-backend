b9b154ef0a7d689b4f99ce01720c465bb30ffffa
Ivanio Martins <ivaniomartins1991@gmail.com>
2022-05-13 17:31:01 -0300

    Inserindo Pedido

--- src/main/java/com/olsystem/spring_curso/domain/PagamentoComCartao.java
+++ src/main/java/com/olsystem/spring_curso/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.olsystem.spring_curso.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.olsystem.spring_curso.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 
 	private static final long serialVersionUID = 1L;
